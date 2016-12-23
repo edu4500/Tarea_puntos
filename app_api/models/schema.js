629fc7a325514dd73ed0c3d72fda2519565e6d4a
eduardo <laloeasg@gmail.com>
2016-12-23 11:22:41 -0500

    correciones

--- app_api/models/schema.js
+++ app_api/models/schema.js
@@ -2,17 +2,10 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-/*
-var comentario_schema = new Schema({
-	fecha_hora:Date,
-	detalle:{type:String,required:true},
-});//*/
-
 var contenido_schema = new Schema({
 	titulo:{type:String,required:true},
 	fecha_hora:Date,
 	cuerpo:{type:String,required:true},
-	//cometarios:[comentario_schema]
 });
 
 mongoose.model("Contenido",contenido_schema);
--- app_client/MyApp.js
+++ app_client/MyApp.js
@@ -103,9 +103,40 @@ var blogPageCrtl = function($location,$routeParams,localData){
 	vm.getData();
 }//*/
 
+var fomart_FH = function(){
+	return function(fecha){
+		var sub = fecha.substr(0,10).split('-');
+		var mes = '';
+		switch(parseInt(sub[1])){
+			case 1 : mes = 'enero';break;
+			case 2 : mes = 'febrero';break;
+			case 3 : mes = 'marzo';break;
+			case 4 : mes = 'abril';break;
+			case 5 : mes = 'mayo';break;
+			case 6 : mes = 'junio';break;
+			case 7 : mes = 'julio';break;
+			case 8 : mes = 'agosto';break;
+			case 9 : mes = 'setiembre';break;
+			case 10 : mes = 'octubre';break;
+			case 11 : mes = "noviembre";break;
+			case 12 : mes = 'diciembre';break;
+		}
+		return sub[0]+' '+mes+' '+sub[2];
+	}
+}
+
+var navigation = function(){
+	return {
+		restrict: 'EA',
+		templateUrl: 'navigation.page.html'
+	};
+}
+
 angular.module('myApp',['ngRoute'])
   .controller('homePageCrtl',homePageCrtl)
   .controller('newPageCrtl',newPageCrtl)
   .controller('blogPageCrtl',blogPageCrtl)
   .service('localData', localData)
+  .filter('fomart_FH',fomart_FH)
+  .directive('navigation', navigation)
   .config(['$routeProvider','$locationProvider',config]);
